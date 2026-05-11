66feec9dc72f9f2350d4f491f20cd0ee0e8611eb
steve-chavez <stevechavezast@gmail.com>
2026-05-11 17:00:58 -0500

    refactor: clear TODO in tests

--- tests/default.nix
+++ tests/default.nix
@@ -4,38 +4,11 @@ with import (builtins.fetchTarball {
     sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
 }) {};
 let
-  ourPg = callPackage ../nix/postgresql {
-    inherit lib;
-    inherit stdenv;
-    inherit fetchurl;
-    inherit makeWrapper;
-    inherit callPackage;
-  };
-  checked-shell-script = callPackage ../nix/checked-shell-script.nix {
-    inherit lib;
-  };
-  xpg = callPackage ../nix/xpg.nix {
-    inherit ourPg;
-    inherit checked-shell-script;
-  };
+  xpgPkgs = import ../default.nix;
   pgsqlcheck15 = callPackage ../nix/plpgsql-check.nix {
-    postgresql = ourPg.postgresql_15;
+    postgresql = xpgPkgs.postgresql_15;
   };
-  # TODO: this is duplicated with root/default.nix
-  xpgWithExtensions =
-    { exts12 ? [] , exts13 ? [] , exts14 ? [], exts15 ? [], exts16? [], exts17? [], exts18? [] } :
-    callPackage ../nix/xpg.nix {
-      inherit ourPg;
-      inherit checked-shell-script;
-      inherit exts12;
-      inherit exts13;
-      inherit exts14;
-      inherit exts15;
-      inherit exts16;
-      inherit exts17;
-      inherit exts18;
-    };
 in
 {
-  xpg = (xpgWithExtensions { exts15 = [ pgsqlcheck15 ]; });
+  xpg = xpgPkgs.xpgWithExtensions { exts15 = [ pgsqlcheck15 ]; };
 }
