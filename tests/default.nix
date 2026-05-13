1716c95ea398d54f433b1848d7921e1842f6a219
steve-chavez <stevechavezast@gmail.com>
2026-05-13 14:44:41 -0500

    refactor: tests follow flake nixpkgs

--- tests/default.nix
+++ tests/default.nix
@@ -1,8 +1,12 @@
-with import (builtins.fetchTarball {
-    name = "24.05"; # May 31 2024
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-    sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-}) {};
+let
+  lock = (builtins.fromJSON (builtins.readFile ../flake.lock)).nodes.nixpkgs.locked;
+  pkgs = import (builtins.fetchTarball {
+    name = lock.rev;
+    url = "https://github.com/${lock.owner}/${lock.repo}/archive/${lock.rev}.tar.gz";
+    sha256 = lock.narHash;
+  }) {};
+in
+with pkgs;
 let
   xpgPkgs = import ../default.nix;
   pgsqlcheck15 = callPackage ../nix/plpgsql-check.nix {
